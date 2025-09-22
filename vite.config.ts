1afe2eb58a1ce52854aba15636e7e22b58391b32
Patrick <patrick.almeida06@gmail.com>
2025-09-22 13:18:10 -0300

    fix: attempt one to fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/8bitsPortifolio",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
