1b2f351f43618b56b5a3cb75a08f6a7342e8bae0
Patrick <patrick.almeida06@gmail.com>
2025-09-22 13:54:11 -0300

    fix: The deploy error

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    minify: "terser"
+  },
   esbuild: {
     minifyIdentifiers: false,
   },
