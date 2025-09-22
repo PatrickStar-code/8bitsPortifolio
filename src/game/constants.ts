4323995b0ee38822e1b22e94fcd6107091e9d2fe
Patrick <patrick.almeida06@gmail.com>
2025-09-22 15:37:03 -0300

    refactor:the map

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -7,10 +7,14 @@ Aqui está o meu <a href="https://github.com/PatrickStar-code" target="_blank">G
   "cs-degree": `Meu diploma de Tecnólogo em Analisé e Desenvolvimento de Sistemas além de um Técnico de Desenvolvimento de Sistemas pelo Senai. Foi uma jornada difícil, mas valeu a pena.`,
   "sofa-table": `Este é meu Sofa. Gosto de relaxar ou jogar algo para passar o tempo. 
 `,
-  tv: `Esta é minha Tv. Eu tenho assistido bastante conteúdo sobre programação aqui como Fireship ou Filipe Deschamps.:`,
+  tv: `Esta é minha Tv. Eu tenho assistido bastante conteúdo sobre programação aqui como Fireship ou Filipe Deschamps.`,
   bed: `Aqui é onde Eu durmo. Dormir é importante para a saúde mental e física.`,
   resume: `Esta é minha mesa e nela está meu currículo. <a href="" target="_blank">Confira?</um> Entre em contato comigo em patrick.almeida06@gmail.com se tiver alguma oportunidade de emprego interessante!`,
   projects: `Em Desenvolvimento aguarde novidades! As sprites vão ser inseridas np projeto conforme eu for desenvolvendo os projetos.`,
   library: `Aqui estão alguns dos meus livros favoritos sobre programação. Eu gosto de ler sobre novos frameworks, linguagens e boas práticas. Mais sendo sincero uso mais a web`,
   exit: `Se quiser sair do portifólio, clique na imagem do Gameboy no canto inferior direito da tela ou simplesmente feche a aba.`,
+  alpaca: `Essa é minha alpaca de estimação. Ela é muito fofa e adora comer grama. Que tal vestila-la no <a href="https://patrickstar-code.github.io/AlpacaGenerator/" target="_blank" className="text-blue-800">Alpaca Generator</a>?`,
+  cat: `Esse é meu gato Simba. Ele ja enfrentou monstros que tal conferir as aventuras dele no <a href="https://github.com/PatrickStar-code/Pixel-Survival" target="_blank" className="text-blue-800">Pixel Survival</a>?`,
+  photo: "Essa é uma foto.Me fez lembrar de um site para um fotografo que fiz, que tal conferir o <a href='https://arthurcesarfotografia.vercel.app/' target='_blank' className='text-blue-800'>PhotoPortfolio</a>?",
+  notebook: "Mais uma prateleira de livros... mas aqui guardo vários diários. Que tal organizar os seus no meu site: <a href='https://notebookly.vercel.app/' target='_blank' className='text-blue-800'>Caderno</a>?",
 };
